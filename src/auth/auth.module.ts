92fdc0e8313a7f0530dc49e8bfec667c30f949af
bigpigmoon <matvey.novikov02@gmail.com>
2022-10-09 20:56:38 +0700

    settings up passport strategies

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { AtStrategy, RtStrategy } from './strategies';
 
 @Module({
+  imports: [AtStrategy, RtStrategy],
   controllers: [AuthController],
   providers: [AuthService]
 })
