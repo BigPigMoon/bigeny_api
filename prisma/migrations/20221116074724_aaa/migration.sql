27fc861d7d44d9ad0edcec56ea2b9392468b7112
bigpigmoon <matvey.novikov02@gmail.com>
2023-03-04 00:42:14 +0700

    new migration

--- prisma/migrations/20221116074724_aaa/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "messages" DROP CONSTRAINT "messages_dialogId_fkey";
-
--- AlterTable
-ALTER TABLE "messages" ALTER COLUMN "dialogId" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "messages" ADD CONSTRAINT "messages_dialogId_fkey" FOREIGN KEY ("dialogId") REFERENCES "dialogs"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- prisma/migrations/20221208132700_/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "users" ALTER COLUMN "avatarImg" DROP NOT NULL;
