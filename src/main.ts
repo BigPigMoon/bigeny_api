094beb22ec400f95bce3d217c8a44e10d9a0ecfa
bigpigmoon <matvey.novikov02@gmail.com>
2023-03-08 18:14:27 +0700

    xomic commit

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
-  // app.setGlobalPrefix('');
 
   app.enableCors({
     credentials: true,
